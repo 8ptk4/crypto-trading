38c125c82065d0dd50e0b21be288e65444f26a28
Patrik Karlsson <putte.karlsson@gmail.com>
2022-10-24 18:53:04 +0200

    ghpages

---
+++ build/static/js/11.4e521282.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcrypto-trading"]=this["webpackJsonpcrypto-trading"]||[]).push([[11],{616:function(t,e,n){"use strict";n.r(e);var r=n(0),a=n.n(r),c={height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",background:"#2f303e",color:"lightgrey"};e.default=function(){return a.a.createElement("div",{style:c},a.a.createElement("h3",null,"4o4 page not found!"))}}}]);
+//# sourceMappingURL=11.4e521282.chunk.js.map
